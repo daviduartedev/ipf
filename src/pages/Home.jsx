9dca6f958306fd07f41fb062fc28375698e45927
daviduartedev <daviduarte.dev@gmail.com>
2026-04-14 20:20:41 -0300

    feat: adicionar banners responsivos de anúncio na home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,18 +14,26 @@ export default function Home() {
   }, []);
 
   return (
-    <section className="posts" id="postsContainer">
-      {posts.map(post => (
-        <div key={post.id} className="post-card" onClick={() => navigate(`/post/${post.id}`)}>
-          <img className="post-img" src={`${import.meta.env.BASE_URL}${post.image}`} alt={post.title} />
-          <div className="post-content">
-            <h3 className="post-title">{post.title}</h3>
-            <p className="post-text">{post.excerpt}</p>
+    <>
+      <section className="posts" id="postsContainer">
+        {posts.map(post => (
+          <div key={post.id} className="post-card" onClick={() => navigate(`/post/${post.id}`)}>
+            <img className="post-img" src={`${import.meta.env.BASE_URL}${post.image}`} alt={post.title} />
+            <div className="post-content">
+              <h3 className="post-title">{post.title}</h3>
+              <p className="post-text">{post.excerpt}</p>
+            </div>
           </div>
-        </div>
-      ))}
-    </section>
+        ))}
+      </section>
+
+      <div className="home-banner">
+        <img className="banner-desktop" src={`${import.meta.env.BASE_URL}img/banners/banner_desktop.png`} alt="Anuncie Aqui" />
+        <img className="banner-mobile" src={`${import.meta.env.BASE_URL}img/banners/banner_mobile.png`} alt="Anuncie Aqui" />
+      </div>
+    </>
   );
+
 
 
 }
